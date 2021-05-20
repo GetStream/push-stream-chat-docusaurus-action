afb62b2045d17f34e59c575c673728260a7c89cb
Vir Desai <virdesai@virs-mbp-2.lan>
2021-05-20 12:44:42 -0400

    use case insensitive checks for match handler

--- find_match_handler.sh
+++ find_match_handler.sh
@@ -1,8 +1,10 @@
 sdk_lowercase_no_space=`echo "$1" | tr '[:upper:]' '[:lower:]'`
 sdk_lowercase_no_space=${sdk_lowercase_no_space/ /}
 
+path_lowercase=`echo "$2" | tr '[:upper:]' '[:lower:]'`
+
 # we need to skip React matching React Native
-if [ "${1/ /}" == "react" ] && [[ "$2" == *native* ]]; then
+if [ "$sdk_lowercase_no_space" == "react" ] && [[ "$path_lowercase" == *reactnative* ]]; then
 	echo "Skipping: $2"
 else
 	rm -rf $2
